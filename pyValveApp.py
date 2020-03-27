1f97ad6a5ffd085b6347cdbaa33e0d5ab36bddeb
stefanprtina <62719050+stefanprtina@users.noreply.github.com>
2020-03-27 23:37:59 +0100

    Add files via upload
    Popravljeno

--- pyValveApp.py
+++ pyValveApp.py
@@ -14,81 +14,79 @@ class pyValveApp(tk.Frame):
 
         parent.title("pyValve v0.2")
         self.frame = tk.Frame(self.parent)
-        labelPodaci = tk.Label(self.parent, text="Osnovni podaci o ventilu",font=fontBold)
-        labelPodaci.place(x=20, y=10)
+        self.labelPodaci = tk.Label(self.parent, text="Osnovni podaci o ventilu",font=fontBold)
+        self.labelPodaci.place(x=20, y=10)
 
         #Lokacija ventila
-        labelLokacijaVentila = tk.Label(self.parent, text="Lokacija ventila:", bd=1, font=fontRegular)
-        labelLokacijaVentila.place(x=30, y=55)
-        entryLokacijaVentila = tk.Entry(self.parent)
-        entryLokacijaVentila.place(x=150, y=50)
+        self.labelLokacijaVentila = tk.Label(self.parent, text="Lokacija ventila:", bd=1, font=fontRegular)
+        self.labelLokacijaVentila.place(x=30, y=55)
+        self.entryLokacijaVentila = tk.Entry(self.parent)
+        self.entryLokacijaVentila.place(x=150, y=50)
 
         #Serijski broj
-        labelSerBrojVentila = tk.Label(self.parent, text="Serijski broj:", bd=1, font=fontRegular)
-        labelSerBrojVentila.place(x=30, y=85)
-        entrySerBrojVentila = tk.Entry(self.parent)
-        entrySerBrojVentila.place(x=150, y=80)
+        self.labelSerBrojVentila = tk.Label(self.parent, text="Serijski broj:", bd=1, font=fontRegular)
+        self.labelSerBrojVentila.place(x=30, y=85)
+        self.entrySerBrojVentila = tk.Entry(self.parent)
+        self.entrySerBrojVentila.place(x=150, y=80)
 
         #Nominalni precnik
-        labelPrecnikVentila = tk.Label(self.parent, text="Nominalni prečnik:", font=fontRegular)
-        labelPrecnikVentila.place(x=30, y=115)
+        self.labelPrecnikVentila = tk.Label(self.parent, text="Nominalni prečnik:", font=fontRegular)
+        self.labelPrecnikVentila.place(x=30, y=115)
 
-        entryPrecnikVentila = tk.Entry(self.parent)
-        entryPrecnikVentila.place(x=150, y=110)
+        self.entryPrecnikVentila = tk.Entry(self.parent)
+        self.entryPrecnikVentila.place(x=150, y=110)
 
         #Radni medijum
-        labelRadniMedijVentila = tk.Label(self.parent, text="Radni medijum:", font=fontRegular)
-        labelRadniMedijVentila.place(x=30, y=145)
+        self.labelRadniMedijVentila = tk.Label(self.parent, text="Radni medijum:", font=fontRegular)
+        self.labelRadniMedijVentila.place(x=30, y=145)
 
-        entryRadniMedijVentila = tk.Entry(self.parent)
-        entryRadniMedijVentila.place(x=150, y=140)
+        self.entryRadniMedijVentila = tk.Entry(self.parent)
+        self.entryRadniMedijVentila.place(x=150, y=140)
 
-        labelIspitniPodaci = tk.Label(self.parent, text="Ispitni podaci", font=fontBold)
-        labelIspitniPodaci.place(x=30, y=180)
+        self.labelIspitniPodaci = tk.Label(self.parent, text="Ispitni podaci", font=fontBold)
+        self.labelIspitniPodaci.place(x=30, y=180)
 
         #Pritisak početka otvaranja
 
-        labelPritisakOtvaranja = tk.Label(self.parent, text="Pritisak otvaranja:", font=fontRegular)
-        labelPritisakOtvaranja.place(x=30, y=215)
+        self.labelPritisakOtvaranja = tk.Label(self.parent, text="Pritisak otvaranja:", font=fontRegular)
+        self.labelPritisakOtvaranja.place(x=30, y=215)
 
-        entryPritisakOtvaranja = tk.Entry(self.parent)
-        entryPritisakOtvaranja.place(x=150, y=210)
+        self.entryPritisakOtvaranja = tk.Entry(self.parent)
+        self.entryPritisakOtvaranja.place(x=150, y=210)
 
         #Ispitni medijum
-        labelRadniMedijVentila = tk.Label(self.parent, text="Ispitni medijum:", font=fontRegular)
-        labelRadniMedijVentila.place(x=30, y=245)
+        self.labelRadniMedijVentila = tk.Label(self.parent, text="Ispitni medijum:", font=fontRegular)
+        self.labelRadniMedijVentila.place(x=30, y=245)
 
-        radniMedijVar = tk.StringVar(self.parent)
-        radniMedijList = ["N2", "Voda"]
-        radniMedijVar.set(radniMedijList[0])
+        self.radniMedijVar = tk.StringVar(self.parent)
+        self.radniMedijList = ["N2", "Voda"]
+        self.radniMedijVar.set(self.radniMedijList[0])
 
-        selRadniMedijVentila = tk.OptionMenu(self.parent, radniMedijVar, *radniMedijList)
-        selRadniMedijVentila.place(x=150, y=240)
+        self.selRadniMedijVentila = tk.OptionMenu(self.parent, self.radniMedijVar, *self.radniMedijList)
+        self.selRadniMedijVentila.place(x=150, y=240)
 
         #Port selection
 
         #label
-        labelPortSel = tk.Label(self.parent, text="Interfejs senzora", font=fontBold)
-        labelPortSel.place(x=420, y=10)
+        self.labelPortSel = tk.Label(self.parent, text="Interfejs senzora", font=fontBold)
+        self.labelPortSel.place(x=420, y=10)
 
         #Serijski port
-        labelSerPort = tk.Label(self.parent, text="Izaberi port", font=fontRegular)
-        labelSerPort.place(x=420, y=40)
+        self.labelSerPort = tk.Label(self.parent, text="Izaberi port", font=fontRegular)
+        self.labelSerPort.place(x=420, y=40)
 
-        serPortVar = tk.StringVar(self.parent)
-        serPortList = [port.device for port in serialCom.getPorts()]
+        self.serPortVar = tk.StringVar(self.parent)
+        self.serPortList = [port.device for port in serialCom.getPorts()]
 
-        if(serPortList):
-            serPortVar.set(serPortList[0])
-        selSerPort = tk.OptionMenu(self.parent, serPortVar, *serPortList)
-        selSerPort.place(x=420, y=70)
+        if(self.serPortList):
+            self.serPortVar.set(self.serPortList[0])
+        self.selSerPort = tk.OptionMenu(self.parent, self.serPortVar, *self.serPortList)
+        self.selSerPort.place(x=420, y=70)
 
-        buttonOpenSerial = tk.Button(text="Konektuj inferfejs", command=lambda: serialCom.open(self, self.parent, serPortVar.get()))
-
-
-        buttonOpenSerial.place(x=470, y=100)
-
+        self.buttonOpenSerial = tk.Button(text="Konektuj inferfejs", command=lambda: serialCom.open(self, self.parent, self.serPortVar.get()))
+        self.buttonOpenSerial.place(x=470, y=100)
         self.frame.pack()
+
 class serialCom():
 
     def open(self, parent, port):
@@ -97,11 +95,11 @@ class serialCom():
         time.sleep(2)
         if(ser.inWaiting()>0):
             print(ser.readline())
-            self.parent.buttonOpenSerial.config(bg="GREEN")
+            self.buttonOpenSerial.config(bg="GREEN")
             return True
         return FALSE
 
-    def getPorts(self):
+    def getPorts():
         portsAvailable = serial.tools.list_ports.comports()
         return portsAvailable
 
